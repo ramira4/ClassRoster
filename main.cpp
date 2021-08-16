21b9c62ef882fcbf48c7ef0638db10c716f45830
ramira4 <80421385+ramira4@users.noreply.github.com>
2021-08-16 08:05:43 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main() {
 	const string studentData[] = { "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Ana,Ramirez,aram457@wgu.edu,23,15,20,25,SOFTWARE" };
 
 	Roster* classRoster = new Roster(studentData, 5);
-	
+	 
 	
 	//F4
 	classRoster->printAll();
