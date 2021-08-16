611b365e031e974bdcdce5d4c295f6b72f2b3231
ramira4 <80421385+ramira4@users.noreply.github.com>
2021-08-16 08:05:22 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ int main() {
 	cout << "Course Title: Scripting and Programming-Applications-c867" << endl;
 	cout << "Programming Language used: C++" << endl;
 	cout << "WGU Student ID: 005430949 " << endl;
-	cout << "Name: Analy Ramirez-Berber" << endl;
+	cout << "Name: Ana Ramirez-Berber" << endl;
 	cout << endl;
 
-	//A.   “studentData Table” 
-	const string studentData[] = { "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Analy,Ramirez,aram457@wgu.edu,23,15,20,25,SOFTWARE" };
+	//A.   â€œstudentData Tableâ€ 
+	const string studentData[] = { "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Ana,Ramirez,aram457@wgu.edu,23,15,20,25,SOFTWARE" };
 
 	Roster* classRoster = new Roster(studentData, 5);
 	
