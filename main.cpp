f188a8b7d42d2962dc1f4bb4e9e7e258b9f8a2e4
Ana Ramirez <80421385+ramira4@users.noreply.github.com>
2021-11-27 23:56:16 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,20 +5,15 @@
 using namespace std;
 
 int main() {
-	//F1 
-	cout << "Course Title: Scripting and Programming-Applications-c867" << endl;
-	cout << "Programming Language used: C++" << endl;
-	cout << "WGU Student ID: 005430949 " << endl;
-	cout << "Name: Ana Ramirez-Berber" << endl;
-	cout << endl;
 
-	//A.   “studentData Table” 
+
+	// “studentData Table” 
 	const string studentData[] = { "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Ana,Ramirez,aram457@wgu.edu,23,15,20,25,SOFTWARE" };
 
 	Roster* classRoster = new Roster(studentData, 5);
 	 
 	
-	//F4
+	//
 	classRoster->printAll();
 	cout << endl;
 	cout << "Printing invalid emails:"<< endl;
